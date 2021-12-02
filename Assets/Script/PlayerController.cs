851d10c9e828cb004a417f55e8c9c5a25754e856
PierreLouisBouchez <pl.bouchez@outlook.fr>
2021-12-03 00:17:37 +0100

    amelioration mouvement

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpheigt;
+    [SerializeField] float sensitive;
+
+    public CharacterController controller;
+    public Transform camera;
+    public Transform feet;
+
+    float rotationX = 0.0f;
+    public float gravity = 9.5f;
+    float fall;
+    bool onGround;
+    public LayerMask ground;
+    private bool isCrouching;
+    private bool isRamping;
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        onGround = Physics.CheckSphere(feet.position,0.4f, ground);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical   = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            vertical *= 1.5f;
+        }
+        if (controller.isGrounded)
+        {
+            fall = -2;
+
+            if (Input.GetButtonDown("Jump") && onGround)
+            {
+                fall = jumpheigt;
+
+            }
+        }
+
+
+        fall -= gravity* Time.deltaTime;
+        Vector3 move = transform.right * horizontal + transform.forward* vertical;
+        move.y = fall;
+
+        controller.Move(move*speed*Time.deltaTime);
+
+        Debug.Log(controller.isGrounded);
+       
+
+
+
+
+
+        float mouseX = Input.GetAxis("Mouse X") * sensitive * Time.deltaTime;
+        rotationX -= Input.GetAxis("Mouse Y") * sensitive * Time.deltaTime;
+
+        
+        rotationX = Mathf.Clamp(rotationX, -60f, 60f);
+
+        camera.localRotation = Quaternion.Euler(rotationX, 0f,0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+
+
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.W))
+        {
+            controller.height = .5f;
+            isCrouching = true;
+            if (Input.GetKey(KeyCode.W))
+            {
+                controller.height = .1f;
+                isRamping = true;
+            }
+        }
+        else
+        {
+            controller.height = 2.5f;
+            isCrouching = false;
+        }
+    }
+}
